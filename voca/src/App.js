12dcf48a6d1a98565e39d62c204cb7462cb67918
asj <asj9674@gmail.com>
2022-07-06 13:36:20 +0900

    state와 props 적용

--- voca/src/App.js
+++ voca/src/App.js
@@ -7,7 +7,9 @@ import Welcome from './component/Welcome';
 function App() {
   return (
     <div className="App">
-      <Hello/>
+      <Hello score={45}/>
+      <Hello score={70}/>
+      <Hello score={95}/>
       {/* <div className = {styles.box}>app.css에 있는 style</div> */}
     </div>
   );
--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -2,21 +2,28 @@ import { useState } from 'react';
 import Name from './Name'
 import styles from './Hello.module.css';
 
-const Hello = () => {
-    const [name, nameChange] = useState('asj');
-
-    function chageName() {
-        const newName = name === 'asj' ? 'hdh' : 'asj';
-        nameChange(newName);
-     }
+export default function Hello(props) {
+    // console.log(props);
+    const [name, nameChange] = useState('안수진');
+    const [score, scoreChange] = useState(props.score);
+    const msg = score > 50 ?  "합격입니다." : "불합격입니다.";
 
     return (
         <div>
-            <span>State</span>
-            <p>{name}</p>
-            <button onClick = {chageName}>Chane Name</button>
+            <span>props</span>
+            <p>{name} / {score}점 : {msg}</p>
+            <button onClick = {() => {
+                nameChange(name === '안수진' ? '홍둥택' : '안수진');
+                
+            }}>Change Name</button>
+            <button onClick = {() => {
+                scoreChange(score + 1);
+            }}>plus</button>
+            <button onClick = {() => {
+                scoreChange(score - 1);
+            }}>Minus</button>
         </div>
     )
 }
 
-export default Hello;
+;
