2bb276f4a8c2d1eb83a5bd5b706d1f1c169d3bc5
asj <asj9674@gmail.com>
2022-07-07 19:40:08 +0900

    잘못된 경로 표시 페이지 추가

--- voca/src/App.js
+++ voca/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Header from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
+import EmptyPage from './component/EmptyPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />}></Route>
           <Route path="/day/:day" element={<Day />}></Route>
+          <Route path="/*" element={<EmptyPage/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
