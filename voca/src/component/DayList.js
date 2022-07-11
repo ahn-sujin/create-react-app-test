0fb13449b9913c2546562630a62a42a2438170ac
asj <asj9674@gmail.com>
2022-07-11 17:59:46 +0900

    json server 연결 및 userEffect 사용

--- voca/src/component/DayList.js
+++ voca/src/component/DayList.js
@@ -1,15 +1,42 @@
-import  {Link} from 'react-router-dom';
-import dummy from '../db/data.json';
+import {Link} from 'react-router-dom';
+import {useState, useEffect} from 'react';
+// import dummy from '../db/data.json';
 
 const DayList = () => {
+    const [days, setDays] = useState([]);    
+    const [count, setCount] = useState(0);
+
+    function onClick1(){
+        setCount(count + 1)
+    }
+
+    function onClick2(){
+        setDays([
+            ...days,
+            {
+                id:Math.random(),
+                day: 1
+            }
+        ])
+    }
+
+    useEffect(()=>{
+        console.log('count change')
+    },[count])
+
     return(
-        <ul className ="list_day">
-            {dummy.days.map(day => (
-                <li key = {day.id}>
-                    <Link to = {`/day/${day.day}`}>{day.day}Day</Link>
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul className ="list_day">
+                {days.map(day => (
+                    <li key = {day.id}>
+                        <Link to = {`/day/${day.day}`}>{day.day}Day</Link>
+                    </li>
+                ))}
+            </ul>
+            <button onClick = {onClick1}>{count}</button>
+            <button onClick = {onClick2}>day change</button>
+        </>
+        
     )
 }
 
